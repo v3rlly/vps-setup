f71dc2bc8ab1be736cb8cca9ba6a407403c6e6d7
v3rlly <pablov3rlly@gmail.com>
2021-05-15 13:03:17 -0700

    Create Basic.sh

---
+++ Basic.sh
@@ -0,0 +1,23 @@
+!/usr/bin/bash
+#
+# Basic configuration for development 
+# author: v3rlly
+
+# update
+sudo apt-get update
+
+# build essential / gcc / g++ / make / cmake
+sudo apt-get install -y build-essential
+sudo apt-get install -y cmake
+
+# Essential
+sudo apt install -y git vim tmux
+sudo apt install -y netcat curl
+sudo apt install -y p7zip-full zip unzip
+sudo apt install -y net-tools dnsutils
+sudo apt install -y libdbus-1-dev
+sudo apt install -y libssl-dev
+sudo apt install -y libssh-dev
+sudo apt install -y lua5.3
+sudo apt install -y htop
+sudo apt install -y tor
