970a21fa4d6ee13ce4bdd43c791762f92bdf5be8
v3rlly <pablov3rlly@gmail.com>
2021-05-16 12:32:00 -0700

    fix sudo permissions

--- Nginx_Redis.sh
+++ Nginx_Redis.sh
@@ -3,5 +3,5 @@
 ########### install nginx and redis ###########
 sudo apt-get install -y nginx redis-server
 # stop and disable both
-sudo systemctl stop nginx.service && systemctl disable nginx.service
+sudo systemctl stop nginx.service && sudo systemctl disable nginx.service
 sudo systemctl stop redis-server.service && sudo systemctl disable redis-server.service
