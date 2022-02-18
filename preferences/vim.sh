b4dda5fd4862fc70e1767a553f08654fcc388bb9
v3rlly <pablov3rlly@gmail.com>
2022-02-18 13:06:33 -0300

    use vimrc.local instead

--- preferences/vim.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-touch ~/.vimrc
-echo ":set number" >> ~/.vimrc
-echo ":set shiftwidth=2" >> ~/.vimrc
-echo ":set tabstop=2" >> ~/.vimrc
-echo ":set autoindent" >> ~/.vimrc
-echo ":set smartindent" >> ~/.vimrc
-echo ":syntax on" >> ~/.vimrc
